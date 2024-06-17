dcfd0b841e2428f50fa49569324663bec348d8fc
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-18 01:24:22 +0800

    Update lirukou.js

--- lirukou.js
+++ lirukou.js
@@ -9,7 +9,7 @@ const customParam = urlParams.get('url');
 const randomString = Math.random().toString(36).substring(2); // 生成随机字符串，包括数字和小写字母
 
 // 构建跳转链接
-let redirectURL = "https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/cbb4a2ac25ba427694e79110f9b302ff.js";
+let redirectURL = "https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/7d6ee339f08c44658056cb900180044c.html";
 if (customParam) {
     redirectURL += "?url=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
 }
